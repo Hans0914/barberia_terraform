b97bb43bf04531fa3017433a7403e8d905c4d47a
Hans0914 <90975901+Hans0914@users.noreply.github.com>
2025-01-10 20:12:30 -0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  default    = ""
+}
